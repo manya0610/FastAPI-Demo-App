a1916fab5ac1abdc5c4f82ad95d0f37ec394e042
Manish Raut <manish@MacBook-Air.local>
2026-03-30 21:38:02 +0530

    more tests

--- tests/src/app/routers/test_user_router.py
+++ tests/src/app/routers/test_user_router.py
@@ -19,23 +19,18 @@ async def test_create_user_201(db_session, async_client:AsyncClient):
     user:UserPublic = await user_fixture(db_session, )
     token = create_access_token(user.id)
     loop = asyncio.get_running_loop()
-    print(f"test event loop {str(loop)},{ loop.is_running()},{threading.current_thread().name}, {type(loop).__name__}")
-
     data = {"name": "test_user_1", "password": "1234"}
     headers = {
         "Authorization" : f"Bearer {token}",
         "Content-Type" : "application/json"
     }
-    print(async_client)
     response = await async_client.post("/user",json=data, headers=headers)
-    print(response.json())
     assert response.status_code == 201
     assert response.json() == {
         "id": 2,
         "name": "test_user_1",
     }
 
-    print("first test done")
 
     # 422 case
     data = {"name": 1}
@@ -44,7 +39,6 @@ async def test_create_user_201(db_session, async_client:AsyncClient):
         "Content-Type" : "application/json"
     }
     response = await async_client.post("/user",json=data, headers=headers)
-    print(response.json())
     assert response.status_code == 422
 
 
@@ -60,5 +54,4 @@ async def test_create_user_401(db_session, async_client:AsyncClient):
         "Content-Type" : "application/json"
     }
     response = await async_client.post("/user",json=data, headers=headers)
-    print(response.json())
     assert response.status_code == 401
